f8a5e8bd993cf4f53707b327e70c384cc0b134cb
Strayko <strayko_lol@windowslive.com>
2019-12-23 10:51:43 +0100

    Rendering Partial Views

---
+++ OdeToFood/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantData _restaurantData;
+
+        public Restaurants Restaurants { get; set; }
+
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurants = _restaurantData.GetById(restaurantId);
+            if (Restaurants == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = _restaurantData.Delete(restaurantId);
+            _restaurantData.Commit();
+
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
