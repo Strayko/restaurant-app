c04243a861487cc11063f28be9705c0519cd8ebc
Strayko <strayko_lol@windowslive.com>
2019-12-26 20:12:10 +0100

    Building Self-contained Applications

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
@@ -13,19 +14,22 @@ namespace OdeToFood.Pages.Restaurants
     {
         private readonly IConfiguration _config;
         private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
         public string Message { get; set; }
         public IEnumerable<Core.Restaurants> Restaurants { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData, ILogger<ListModel> logger)
         {
             _config = config;
             _restaurantData = restaurantData;
+            _logger = logger;
         }
         public void OnGet()
         {
+            _logger.LogError("Executing ListModel");
             Message = _config["Message"];
             Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
         }
